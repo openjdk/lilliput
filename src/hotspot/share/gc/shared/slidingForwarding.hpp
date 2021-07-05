04da65e6b818fe3e5a26be3a4557800d7dba34e8
Roman Kennke <rkennke@redhat.com>
2021-07-05 18:31:06 +0200

    Keep self-forwarded header bit free for self-forwarding

--- src/hotspot/share/gc/shared/slidingForwarding.hpp
+++ src/hotspot/share/gc/shared/slidingForwarding.hpp
@@ -70,8 +70,8 @@ private:
 
   static const uintptr_t ONE = 1ULL;
 
-  // We need the lowest two bits to indicate a forwarded object.
-  static const int BASE_SHIFT = 2;
+  // We need the lowest three bits to indicate a forwarded object and self-forwarding.
+  static const int BASE_SHIFT = 3;
 
   // The compressed address bits start here.
   static const int COMPRESSED_BITS_SHIFT = BASE_SHIFT + NUM_REGION_BITS;
