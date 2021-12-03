e131a3dd6a6e25090a61b113a4bc0111b8cd0816
Roman Kennke <rkennke@redhat.com>
2021-12-03 16:31:26 +0100

    Use offset 4 for C2 load-klass type system

--- src/hotspot/share/oops/oop.hpp
+++ src/hotspot/share/oops/oop.hpp
@@ -307,6 +307,7 @@ class oopDesc {
   // for code generation
   static int mark_offset_in_bytes()      { return offset_of(oopDesc, _mark); }
   static int klass_offset_in_bytes()     { return offset_of(oopDesc, _metadata._klass); }
+  static int nklass_offset_in_bytes()     { return 4; }
   static int klass_gap_offset_in_bytes() {
     assert(has_klass_gap(), "only applicable to compressed klass pointers");
     return klass_offset_in_bytes() + sizeof(narrowKlass);
