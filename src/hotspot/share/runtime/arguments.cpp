e59d14d62ca2a90d54b8779a7bea8adc8a7a5ffc
Roman Kennke <rkennke@amazon.de>
2023-02-20 20:20:57 +0000

    Revert condition to turn off CDS when Lilliput is disabled

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -1566,7 +1566,7 @@ void Arguments::set_use_compressed_klass_ptrs() {
     }
     FLAG_SET_DEFAULT(UseCompressedClassPointers, true);
   }
-  if (UseCompactObjectHeaders) {
+  if (!UseCompactObjectHeaders) {
     FLAG_SET_DEFAULT(UseSharedSpaces, false);
   }
 
