40d889bc2615685f5804f9024d0f2a926b2dae76
Roman Kennke <rkennke@amazon.de>
2023-04-13 11:58:13 +0000

    Ensure compaction order in serial GC

--- src/hotspot/share/gc/shared/forwardingTable.inline.hpp
+++ src/hotspot/share/gc/shared/forwardingTable.inline.hpp
@@ -84,7 +84,7 @@ inline void PerRegionTable::reforward(HeapWord* from, HeapWord* to) {
 
 inline void PerRegionTable::forward_to(HeapWord* from, HeapWord* to) {
   if (_insertion_idx > 0 && _table[_insertion_idx - 1].from() >= from) {
-    assert(UseG1GC, "happens only with G1 serial compaction");
+    assert(UseG1GC, "happens only with G1 serial compaction, _insertion_idx: " INTX_FORMAT ", entry: " PTR_FORMAT " from: " PTR_FORMAT, _insertion_idx, p2i(_table[_insertion_idx-1].from()), p2i(from));
     reforward(from, to);
     return;
   }
