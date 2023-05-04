0a3ef0270aa90bd7c0b2a00a0c491e569119861b
Roman Kennke <rkennke@amazon.com>
2023-05-04 11:26:36 +0000

    Cleanup

--- src/hotspot/share/gc/shenandoah/shenandoahConcurrentGC.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahConcurrentGC.cpp
@@ -771,7 +771,6 @@ public:
   void work(uint worker_id) {
     ShenandoahConcurrentWorkerSession worker_session(worker_id);
     {
-      ShenandoahSuspendibleThreadSetJoiner sts_join;
       ShenandoahEvacOOMScope oom;
       // jni_roots and weak_roots are OopStorage backed roots, concurrent iteration
       // may race against OopStorage::release() calls.
