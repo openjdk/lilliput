e58cf268663d6638a4cc5a31dd3b4afe1cf1599b
tstuefe <thomas.stuefe@gmail.com>
2023-08-15 09:36:15 +0200

    Post-Merge fixes

--- src/hotspot/share/oops/markWord.hpp
+++ src/hotspot/share/oops/markWord.hpp
@@ -26,6 +26,7 @@
 #define SHARE_OOPS_MARKWORD_HPP
 
 #include "metaprogramming/primitiveConversions.hpp"
+#include "oops/compressedKlass.hpp" // for narrowKlass
 #include "oops/oopsHierarchy.hpp"
 #include "runtime/globals.hpp"
 
