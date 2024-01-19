c141dff3e3e09bed034d63d30969093c407b08ee
tstuefe <thomas.stuefe@gmail.com>
2024-01-19 15:24:59 +0100

    fix  runtime/cds/appcds/CommandLineFlagComboNegative.java

--- test/hotspot/jtreg/runtime/cds/appcds/CommandLineFlagComboNegative.java
+++ test/hotspot/jtreg/runtime/cds/appcds/CommandLineFlagComboNegative.java
@@ -66,9 +66,9 @@ public class CommandLineFlagComboNegative {
                 "An error has occurred while processing the shared archive file", 1) );
         }
         testTable.add( new TestVector("-XX:+UseCompressedOops", "-XX:-UseCompressedOops",
-            "The saved state of UseCompressedOops and UseCompressedClassPointers is different from runtime, CDS will be disabled.", 1) );
+            "The saved state of UseCompressedOops + UseCompressedClassPointers + UseTinyClassPointers is different from runtime, CDS will be disabled.", 1) );
         testTable.add( new TestVector("-XX:+UseCompressedClassPointers", "-XX:-UseCompressedClassPointers",
-           "The saved state of UseCompressedOops and UseCompressedClassPointers is different from runtime, CDS will be disabled.", 1) );
+           "The saved state of UseCompressedOops + UseCompressedClassPointers + UseTinyClassPointers is different from runtime, CDS will be disabled.", 1) );
     }
 
     private void runTests() throws Exception
