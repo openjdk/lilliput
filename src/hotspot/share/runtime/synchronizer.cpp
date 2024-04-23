611ff1a70ffe2ee631d139ec276eefe30f152f61
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2024-04-23 10:09:30 +0000

    Use cache instead of table

--- src/hotspot/share/runtime/synchronizer.cpp
+++ src/hotspot/share/runtime/synchronizer.cpp
@@ -430,11 +430,20 @@ bool ObjectSynchronizer::quick_enter(oop obj, JavaThread* current,
   const markWord mark = obj->mark();
 
   if (mark.has_monitor()) {
-    ObjectMonitor* const m = ObjectSynchronizer::read_monitor(current, obj, mark);
+    ObjectMonitor* m = nullptr;
+    if (LockingMode == LM_LIGHTWEIGHT) {
+      m = current->om_get_from_monitor_cache(obj);
+      if (m == nullptr) {
+        // Take the slow-path on a cache miss.
+        return false;
+      }
+    } else {
+      m = ObjectSynchronizer::read_monitor(mark);
+    }
     // An async deflation or GC can race us before we manage to make
     // the ObjectMonitor busy by setting the owner below. If we detect
     // that race we just bail out to the slow-path here.
-    if (m == nullptr || m->object_peek() == nullptr) {
+    if (m->object_peek() == nullptr) {
       return false;
     }
     JavaThread* const owner = static_cast<JavaThread*>(m->owner_raw());
