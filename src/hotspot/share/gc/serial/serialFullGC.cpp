33d90b2ced7f07baab28846ce4afbcc132a31ab6
Roman Kennke <rkennke@openjdk.org>
2025-01-22 11:46:42 +0000

    8346011: [Lilliput] Compact Full-GC Forwarding
    Reviewed-by: stuefe

--- src/hotspot/share/gc/serial/serialFullGC.cpp
+++ src/hotspot/share/gc/serial/serialFullGC.cpp
@@ -697,6 +697,8 @@ void SerialFullGC::invoke_at_safepoint(bool clear_all_softrefs) {
 
   phase1_mark(clear_all_softrefs);
 
+  FullGCForwarding::begin();
+
   Compacter compacter{gch};
 
   {
@@ -739,6 +741,8 @@ void SerialFullGC::invoke_at_safepoint(bool clear_all_softrefs) {
   }
 
   restore_marks();
+
+  FullGCForwarding::end();
 
   deallocate_stacks();
 
