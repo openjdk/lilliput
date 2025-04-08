163798bbb131faaef28f674354190dbd62c26d03
Roman Kennke <rkennke@amazon.com>
2025-04-08 11:22:21 +0000

    Revert "Avoid racy update in Klass::expand_for_hash()"
    This reverts commit 95728b46ce8be9610988ab61ebe35235bb1f23ff.

--- src/hotspot/share/oops/klass.hpp
+++ src/hotspot/share/oops/klass.hpp
@@ -794,7 +794,7 @@ public:
   virtual int hash_offset_in_bytes(oop obj) const = 0;
   static int kind_offset_in_bytes() { return (int)offset_of(Klass, _kind); }
 
-  bool expand_for_hash(oop obj, markWord m) const;
+  bool expand_for_hash(oop obj) const;
 };
 
 #endif // SHARE_OOPS_KLASS_HPP
